3a49bdff65b418ae604d11b94834f969d6357e13
jadhavrahul01 <jadhavrahul01@gmail.com>
2026-02-09 13:14:47 +0000

    Refactor SitemapController to enhance XML structure and remove unused code

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Blog;
-use App\Models\Page;
 use Illuminate\Http\Response;
 
 class SitemapController extends Controller
@@ -11,44 +10,54 @@ class SitemapController extends Controller
     public function index()
     {
         $blogs = Blog::published()->get();
-        $pages = Page::all();
+        $currentDate = now()->toAtomString();
 
         $xml = '<?xml version="1.0" encoding="UTF-8"?>';
-        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
 
         // Homepage
         $xml .= '<url>';
         $xml .= '<loc>https://bingeatmedia.com/</loc>';
-        $xml .= '<changefreq>daily</changefreq>';
-        $xml .= '<priority>1.0</priority>';
+        $xml .= '<lastmod>' . $currentDate . '</lastmod>';
+        $xml .= '<priority>1.00</priority>';
         $xml .= '</url>';
 
-        // Blog listing page
-        $xml .= '<url>';
-        $xml .= '<loc>https://bingeatmedia.com/blog</loc>';
-        $xml .= '<changefreq>daily</changefreq>';
-        $xml .= '<priority>0.8</priority>';
-        $xml .= '</url>';
+        // Static pages
+        $staticPages = [
+            '/contact' => '0.80',
+            '/portfolio' => '0.80',
+            '/blog' => '0.80',
+        ];
+
+        foreach ($staticPages as $url => $priority) {
+            $xml .= '<url>';
+            $xml .= '<loc>https://bingeatmedia.com' . $url . '</loc>';
+            $xml .= '<lastmod>' . $currentDate . '</lastmod>';
+            $xml .= '<priority>' . $priority . '</priority>';
+            $xml .= '</url>';
+        }
 
         // Individual blog posts
         foreach ($blogs as $blog) {
             $xml .= '<url>';
             $xml .= '<loc>https://bingeatmedia.com/blog/' . htmlspecialchars($blog->slug) . '</loc>';
             $xml .= '<lastmod>' . $blog->updated_at->toAtomString() . '</lastmod>';
-            $xml .= '<changefreq>weekly</changefreq>';
-            $xml .= '<priority>0.7</priority>';
+            $xml .= '<priority>0.80</priority>';
             $xml .= '</url>';
         }
 
-        // Pages
-        foreach ($pages as $page) {
-            $xml .= '<url>';
-            $xml .= '<loc>https://bingeatmedia.com/' . htmlspecialchars($page->slug) . '</loc>';
-            $xml .= '<lastmod>' . $page->updated_at->toAtomString() . '</lastmod>';
-            $xml .= '<changefreq>monthly</changefreq>';
-            $xml .= '<priority>0.6</priority>';
-            $xml .= '</url>';
-        }
+        // Terms and Privacy pages
+        $xml .= '<url>';
+        $xml .= '<loc>https://bingeatmedia.com/terms</loc>';
+        $xml .= '<lastmod>' . $currentDate . '</lastmod>';
+        $xml .= '<priority>0.80</priority>';
+        $xml .= '</url>';
+
+        $xml .= '<url>';
+        $xml .= '<loc>https://bingeatmedia.com/privacy</loc>';
+        $xml .= '<lastmod>' . $currentDate . '</lastmod>';
+        $xml .= '<priority>0.80</priority>';
+        $xml .= '</url>';
 
         $xml .= '</urlset>';
 
